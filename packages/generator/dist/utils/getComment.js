6798dcb24a9c949318a93bf85a1f02434a53472e
{authemail@qq.com} <authemail@qq.com>
2023-04-30 18:24:55 +0800

    chore: update actions

--- packages/generator/dist/utils/getComment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getComment = void 0;
-const getComment = (comment) => {
-    return comment ? `"""\n ${comment} \n"""\n` : '';
-};
-exports.getComment = getComment;
-//# sourceMappingURL=getComment.js.map
